62d581635e818f9e37c1864ca2870cbd4a5dd4a3
Richard Kiss <him@richardkiss.com>
2014-12-05 22:20:05 -0800

    First crack at data.js.

---
+++ data.js
@@ -0,0 +1,32 @@
+var DB = [
+    {
+        peeps: ["Justin Bieber", "Paula Dean", "Miley Cyrus"],
+        answer: "KFP",
+        e_kill: "There’s only one choice. It’s Bieber. Boy’s gotta die. But he’s Canadian so really you should feel no qualms.",
+        e_fuck: "Yes, yes, I know you want to fuck Miley Cyrus. But you CANNOT let Paula be President! She’ll bring back black and white water fountains—but the water fountains will be liquid butter.  So, sorry, you’re stuck playing hide the sausage with Paula—just make sure you pre-heat the oven: things are gonna get hot.",
+        e_president: "So Miley is the president! The national bird is now a tongue! It’s a party in the USA."
+    },
+/*    , 'KFP'],
+        peeps: ['James Franco', 'Seth Rogen', 'Willie Nelson'],
+    ['Kristin Stewart', 'Charlie Sheen', 'Paris Hilton'],
+    ['Kim Kardashian', 'Kendall Kardashian', 'A Gerbil'],
+    ['Bethenny Frankel', 'Frank Underwood', 'Fran Drescher'],
+    ['Nick Cage', 'Nick Carter', 'Vladimir Putin'],
+    ['Jack Nicholson', 'Jack the Ripper', 'Jabba the Hut'],
+    ['Chuck Norris', 'The Most Interesting Man in the World', 'Rand Paul'],
+    ['Chris Brown', 'Charlie Brown', 'Chris Christie'],
+    ['Zooey Deschanel', 'Mindy Kaling', 'Marco Rubio'],
+    ['Pam Anderson', 'Sarah Palin', 'Phoebe (Friends)'],
+    ['Katherine Heigl', 'Stephanie Meyer', 'Lil Wayne'],
+    ['Dog the Bounty Hunter', 'Clifford the Big Red Dog', 'Grumpy Cat'],
+    ['Tyler Perry', 'Katy Perry', 'Bilbo Baggins'],
+    ['Sandra Bullock', 'Sandy Cohen (The OC)', 'Sandy (Grease)'],
+    ['Lance Armstrong', 'Derrick Jeter', 'Dolly the Sheep'],
+    ['Anthony Weiner', 'Carlos Danger', 'George Clooney'],
+    ['Jennifer Lawrence', 'Olivia Wilde', 'Hillary Clinton'],
+    ['A Badger', 'A Raccoon', 'A Skunk'],
+    ['Tia Mowry', 'Tamara Mowry', 'Taj Mowry'],
+    ['Maleficent', 'Cruella Deville', 'Sarah Palin'],
+    ['Courtney Love', 'Jennifer Love Hewitt', 'Rebecca Black']
+*/
+];
