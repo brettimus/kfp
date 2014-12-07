0e71fbd40f5f8a737473355fea204f52bb2d8780
Richard Kiss <him@richardkiss.com>
2014-12-06 16:38:17 -0800

    Update data, elect => marry.

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,8 +1,8 @@
 var DB = [
     {
         peeps: [
-            { name:"Justin Bieber", title:"Beeb" },
-            { name:"Miley Cyrus", title:"Wreck" },
+            { name:"Justin Bieber", title:"Felon" },
+            { name:"Miley Cyrus", title:"Tongue Enthusiast" },
             { name:"Paula Deen", title:"Racist" },
         ],
         answer: "KFP",
@@ -12,20 +12,23 @@ var DB = [
     },
     {
         peeps: [
-            { name:"Voldemort", title:"Distributed Key-Value Storage System" },
-            { name:"Jabba the Hutt", title:"Obese Thing" },
-            { name:"Pepsi Can", title:"Delight" },
+            { name:"Dogg the Bounty Hunter", title:"Bounty Hunter" },
+            { name:"Clifford the Big Red Dog", title:"Large Scarlet Canine" },
+            { name:"Grumpy Cat", title:"Grumpy Cat" },
         ],
-        answer: "KFP",
-        e_0: "There's only one choice. It's Bieber. Boy's gotta die. But he's Canadian so really you should feel no qualms.",
-        e_1: "Yes, yes, I know you want to fuck Miley Cyrus. But you CANNOT let Paula be President! She'll bring back black and white water fountains -- but the water fountains will be liquid butter.  So, sorry, you're stuck playing hide the sausage with Paula -- just make sure you pre-heat the oven: things are gonna get hot.",
-        e_2: "So Miley is the president! The national bird is now a tongue! It's a party in the USA."
     },
     {
         peeps: [
             { name:"Troy Carter", title:"Founder, Chairman and CEO" },
             { name:"Rhett Link", title:"Intertainers" },
-            { name:"Felicia Day", title:"Does Everything: Dragons, Actress, Singer, Creative Chief Officer..." }
+            { name:"Felicia Day", title:"Everything. Seriously, she does everything. She does so much shit, it won't even fit on one line. She's amazing." }
+        ],
+    },
+    {
+        peeps: [
+            { name:"Voldemort", title:"Distributed Key-Value Database" },
+            { name:"Jabba the Hutt", title:"Crime Boss, Slug" },
+            { name:"Pepsi Can", title:"Perfect™" },
         ],
         answer: "KFP",
         e_0: "There's only one choice. It's Bieber. Boy's gotta die. But he's Canadian so really you should feel no qualms.",
